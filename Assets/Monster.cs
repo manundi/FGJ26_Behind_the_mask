6969fac4ba53a908f9b484ff63987208c9d0b5df
miika anundi <manundi@gmail.com>
2026-01-31 19:26:44 +0200

    monster speed fix

--- Assets/Monster.cs
+++ Assets/Monster.cs
@@ -8,6 +8,7 @@ public class Monster : MonoBehaviour
     public float stepTimer = 0.0f;
 
     public float speedNotInSight = 2.0f;
+    public float speedInSight = 1.0f;
 
     public float monsterInSightStep = 1.0f;
 
@@ -18,6 +19,8 @@ public class Monster : MonoBehaviour
     Vector3 targetPosition;
     Animator animator;
     Vector3 lastPos;
+    float currentVelocity = 0.0f;
+
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -35,29 +38,47 @@ public class Monster : MonoBehaviour
     {
         stepTimer += Time.fixedDeltaTime;
 
-        if (stepTimer >= moveTimeStep)
+        
+        if (stepTimer >= moveTimeStep )
         {
             
             stepTimer = 0.0f;
-            targetPosition = transform.position + new Vector3(0, 0,  monsterInSightStep);
+            currentVelocity =speedInSight;
             
         }
-    }
+
+        if(!monsterInSight)
+        {
+            currentVelocity = speedNotInSight;
+        }
+        
+        if( currentVelocity > 0.0f)
+        {
+            currentVelocity -= Time.fixedDeltaTime * 5.0f; 
+        }
+        
+        
+}
 
     void Update()
     {
+        // when monster becomes to sight it should slowly stop not instantly
+
+
        
         if (monsterInSight)
         {
-            transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime ); 
+            transform.position = Vector3.Lerp(transform.position,transform.position + Vector3.forward, Time.deltaTime * currentVelocity ); 
         }
         else
         {
-            transform.position = Vector3.Lerp(transform.position, transform.forward, Time.deltaTime *speedNotInSight); 
+            transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.forward, Time.deltaTime *speedNotInSight); 
             targetPosition = transform.position;
         }
         animator.SetFloat("speed", (transform.position - lastPos).magnitude / Time.deltaTime);
         lastPos = transform.position;
 
     }
+
+   
 }
